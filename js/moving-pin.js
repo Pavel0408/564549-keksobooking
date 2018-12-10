9e80c4692c3794a42a69d8bb6c4e09bc728bf1cd
Pavel <pavel0408@gmail.com>
2018-12-10 14:47:00 +0300

    черновой вариант программы

--- js/moving-pin.js
+++ js/moving-pin.js
@@ -1,6 +1,33 @@
 'use strict';
 (function () {
   var MAIN_PIN = document.querySelector('.map__pin--main');
+  var ADRESS_INPUT = document.querySelector('#address');
+  var MAP_PIN_WEIGHT = 60;
+  var MAP_PIN_HEIGHT = 80;
+  var X_COORDS = {
+    min: 250,
+    max: 1150
+  };
+  var Y_COORDS = {
+    min: 130,
+    max: 630
+  };
+  var isOnMap = function (num, obj) {
+    if (num < obj.min) {
+      return obj.min;
+    }
+    if (num > obj.max) {
+      return obj.max;
+    }
+    return num;
+  };
+
+  var getAdress = function () {
+    var left = +MAIN_PIN.offsetLeft + MAP_PIN_WEIGHT / 2;
+    var top = +MAIN_PIN.offsetTop + MAP_PIN_HEIGHT;
+    ADRESS_INPUT.value = '' + left + ', ' + top;
+  };
+
 
   MAIN_PIN.addEventListener('mousedown', function (evt) {
     console.log(evt);
@@ -21,8 +48,9 @@
         y: moveEvt.clientY
       };
 
-      MAIN_PIN.style.top = (MAIN_PIN.offsetTop - shift.y) + 'px';
-      MAIN_PIN.style.left = (MAIN_PIN.offsetLeft - shift.x) + 'px';
+      MAIN_PIN.style.top = isOnMap(MAIN_PIN.offsetTop - shift.y, Y_COORDS) + 'px';
+      MAIN_PIN.style.left = isOnMap(MAIN_PIN.offsetLeft - shift.x, X_COORDS) + 'px';
+      getAdress();
     };
 
     var onMouseUp = function () {
