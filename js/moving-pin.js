19537ad51d609646380b2304f15c0f5b78311143
Pavel <pavel0408@gmail.com>
2018-12-10 13:36:01 +0300

    пин перемещается

--- js/moving-pin.js
+++ js/moving-pin.js
@@ -2,7 +2,8 @@
 (function () {
   var MAIN_PIN = document.querySelector('.map__pin--main');
 
-  MAIN_PIN.addEventListener('musedown', function (evt) {
+  MAIN_PIN.addEventListener('mousedown', function (evt) {
+    console.log(evt);
     var startCoords = {
       x: evt.clientX,
       y: evt.clientY
@@ -10,21 +11,26 @@
     console.log(startCoords);
 
     var onMouseMove = function (moveEvt) {
-      moveEvt.preventDafault();
+      moveEvt.preventDefault();
       var shift = {
         x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y = moveEvt.clientY
+        y: startCoords.y - moveEvt.clientY
       };
       startCoords = {
-        x: evt.clientX,
-        y: evt.clientY
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
       };
 
       MAIN_PIN.style.top = (MAIN_PIN.offsetTop - shift.y) + 'px';
       MAIN_PIN.style.left = (MAIN_PIN.offsetLeft - shift.x) + 'px';
+    };
 
+    var onMouseUp = function () {
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
     };
     document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
 
   });
 })();
